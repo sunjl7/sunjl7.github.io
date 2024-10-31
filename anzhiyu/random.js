f6f7d16b2b86ee80bf083a4578652adb90695025
sunjl7 <sunjl7@qq.com>
2024-11-01 03:04:12 +0800

    Site updated: 2024-11-01 03:04:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/01/hello-world/","2024/11/01/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/11/01/更新模板后的文章/","2024/11/01/这是一篇新的博文/","2024/11/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
